cd2552495fd437b07aa1ae250fd7911e1ec436a1
OmarMallouk <mr.mallouk.om@gmail.com>
2024-10-30 22:39:31 +0200

    added level3 and its preload

---
+++ scripts/level3.js
@@ -0,0 +1,9 @@
+function preloadLevel3(window) {
+	window.load.image('arrows', 'assets/sprites/projectiles/arrow.png'); 
+	window.load.tilemapTiledJSON("map", "assets/tilemaps/level3.json");
+	window.load.image(
+		"tiles",
+		"assets/sprites/Environment/dungeon.png"
+	);
+	
+}
